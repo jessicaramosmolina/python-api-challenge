6e0ba3bf2d934eedd8bc2bb72e8a401cceaf2ebc
Jessica Ramos Molina <jessicaramosmolina@Jessicas-Air.hsd1.ca.comcast.net>
2020-11-18 17:48:06 -0800

    second commit

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = 'f2deb80b0bea75908cebab9ed8582510'
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = 'AIzaSyCn5p2NDEjlDDfLESzUlsRtI_gnGCNbvnQ'
